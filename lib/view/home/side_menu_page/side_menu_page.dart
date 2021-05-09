693e3dc0dfc487ca7a79c187349a06248d8947be
artahc <28035000+artahc@users.noreply.github.com>
2021-05-09 23:37:24 +0800

    working on grouppable server item

--- lib/view/home/side_menu_page/side_menu_page.dart
+++ lib/view/home/side_menu_page/side_menu_page.dart
@@ -137,7 +137,6 @@ class _ServerListState extends State<_ServerList> {
             ),
             Overlay(
               initialEntries: [
-                Overl
               ],
             ),
             Container(
