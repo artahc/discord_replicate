18338589b59f28487540660a6aa875438f9e81a2
Arta Wibawa <artahace@gmail.com>
2022-01-19 02:57:34 +0800

    added repository test, clean and simple (for success case)

---
+++ test/repository_test.dart
@@ -0,0 +1,64 @@
+import 'package:discord_replicate/model/channel.dart';
+import 'package:discord_replicate/model/credential.dart';
+import 'package:discord_replicate/model/server.dart';
+import 'package:discord_replicate/model/user.dart';
+import 'package:discord_replicate/repository/channel_repository.dart';
+import 'package:discord_replicate/repository/server_repository.dart';
+import 'package:discord_replicate/repository/user_repository.dart';
+import 'package:discord_replicate/service/graphql_client_helper.dart';
+import 'package:discord_replicate/service/hive_database_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:fixnum/fixnum.dart';
+
+class MockDb extends Mock implements HiveDatabaseService {}
+
+main() {
+  var mockDb = MockDb();
+  var api = GraphQLClientHelper(
+    url: "http://localhost:4000/graphql",
+    tokenProvider: () => Future.value(Credential(email: "", token: "", uid: "")),
+    defaultHeader: {
+      "allow-me-in": "artahc123",
+    },
+  );
+
+  group("Remote source", () {
+    tearDown(() {
+      reset(mockDb);
+    });
+
+    test("Load server from remote API, should be able parse result to Server model.", () async {
+      var serverRepo = ServerRepository(apiClient: api, database: mockDb);
+
+      when(() => mockDb.save(any(), any())).thenAnswer((invocation) => Future.value(null));
+      when(() => mockDb.load<Server?>(any())).thenAnswer((invocation) => Future.value(null));
+
+      var server = await serverRepo.load("JkBxr0EoQOYyDeXagC2h");
+
+      expect(server, isA<Server>());
+    });
+
+    test('Load user from remote API, should be able to parse result to Server model.', () async {
+      var userRepo = UserRepository(apiClient: api, database: mockDb);
+
+      when(() => mockDb.save(any(), any())).thenAnswer((invocation) => Future.value(null));
+      when(() => mockDb.load<User?>(any())).thenAnswer((invocation) => Future.value(null));
+
+      var user = await userRepo.load("FMYbWPwFWgTvRemhbbz1dLL9HkC2");
+
+      expect(user, isA<User>());
+    });
+
+    test('Load channel from remote API, should be able to parse result to Channel model.', () async {
+      var channelRepo = ChannelRepository(apiClient: api, database: mockDb);
+
+      when(() => mockDb.save(any(), any())).thenAnswer((invocation) => Future.value(null));
+      when(() => mockDb.load<Channel?>(any())).thenAnswer((invocation) => Future.value(null));
+
+      var channel = await channelRepo.load("65l2SQFgkqYRXXJyRfWT");
+
+      expect(channel, isA<Channel>());
+    });
+  });
+}
